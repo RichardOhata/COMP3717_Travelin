cf197edd97a07017b7238725525b1d5a00082258
RichardOhata <richardohata@gmail.com>
2022-10-06 13:36:21 -0700

    Created FlightInput Page and added some design elements in MainActivity

---
+++ app/src/main/java/com/example/travelin/FlightInput.java
@@ -0,0 +1,14 @@
+package com.example.travelin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlightInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_input);
+    }
+}
--- app/src/main/java/com/example/travelin/MainActivity.java
+++ app/src/main/java/com/example/travelin/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.travelin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hi
+        Button signInBtn = findViewById(R.id.sign_in_btn);
+        signInBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FlightInput.class);
+            startActivity(intent);
+        });
     }
 }
