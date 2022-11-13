e10d6e00da0c4c17984f001ff8cab6e75fd1852c
RichardOhata <richardohata@gmail.com>
2022-11-12 22:37:39 -0800

    Added code for reading from FlightLabs API in FlightInput

--- app/src/main/java/com/example/travelin/FlightInput.java
+++ app/src/main/java/com/example/travelin/FlightInput.java
@@ -3,11 +3,36 @@ package com.example.travelin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FlightInput extends AppCompatActivity {
-
+    Bundle bundle;
+    String username;
+    EditText flightNum, airline_name;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private final String url = "https://app.goflightlabs.com/flights?access_key";
+    private final String appid = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiY2UyNjVlNjAzZTBhYjdkMjEwOWU4MWE1MTU5Y2VkNWNhYTAxZTlkZGRjNDcxNzVmZmE2ODRjOGM0MjQ4NjIzZWViNDYyZDI3ZWUyY2E5N2QiLCJpYXQiOjE2Njc4NDQwMDQsIm5iZiI6MTY2Nzg0NDAwNCwiZXhwIjoxNjk5MzgwMDA0LCJzdWIiOiIxNzMyNCIsInNjb3BlcyI6W119.Si0XteCDaR2RNZExlf3431TDq0L8gwSyppuGVhIDJpi5Lm_YWVimrbVKHGJiDtIcXmaEdteApum3zYFs8X8WIQ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +41,47 @@ public class FlightInput extends AppCompatActivity {
         setContentView(R.layout.activity_flight_input);
 
         Button submitBtn = findViewById(R.id.submitBtn);
+        flightNum = findViewById(R.id.flight_num_editText);
+        airline_name = findViewById(R.id.airline_name_editText);
         submitBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, LandingPage.class);
-            startActivity(intent);
+            String tempURL = url + "=" + appid + "&flight_number=" + flightNum.getText().toString() + "&airline_name=" + airline_name.getText().toString();
+            if (!TextUtils.isEmpty(flightNum.getText().toString()) && !TextUtils.isEmpty(airline_name.getText().toString())) {
+                AsyncTaskRunner runner = new AsyncTaskRunner();
+                runner.execute(tempURL);
+                this.finish();
+            } else {
+                Toast.makeText(FlightInput.this, "Enter flight number and airline name", Toast.LENGTH_SHORT).show();
+            }
         });
+    }
+
+    /**
+     * Gets the user input from the editTexts and checks in the FlightLabs API for
+     * matching flights. If found, store user input in Firebase.
+     */
+    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            RequestQueue queue = Volley.newRequestQueue(FlightInput.this);
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, strings[0], null, response -> {
+                try {
+                    response.getJSONObject(0);
+                    String id = databaseReference.push().getKey();
+                    assert id != null;
+                    Task<Void> setValueTask = databaseReference.child("Flights").child(id).setValue(flightNum.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("This happened");
+                    Toast.makeText(FlightInput.this, "Flight does not exist", Toast.LENGTH_SHORT).show();
+                }
+            });
+            queue.add(request);
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/travelin/LandingPage.java
+++ app/src/main/java/com/example/travelin/LandingPage.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class LandingPage extends AppCompatActivity {
 Button checkFlights;
 Button checkWeather;
+Button inputFlight;
 TextView userGreeting;
 Bundle bundle;
     @Override
@@ -20,6 +21,7 @@ Bundle bundle;
 
         checkFlights = findViewById(R.id.flightsSeeMoreBtn);
         checkWeather = findViewById(R.id.weatherSeeMoreBtn);
+        inputFlight = findViewById(R.id.inputFlight_btn);
         userGreeting = findViewById(R.id.heyThere);
         displayUserGreeting();
         checkFlights.setOnClickListener(view -> {
@@ -28,6 +30,11 @@ Bundle bundle;
         });
         checkWeather.setOnClickListener(view -> {
             Intent intent = new Intent(this, WeatherDetails.class);
+            startActivity(intent);
+        });
+        inputFlight.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FlightInput.class);
+            intent.putExtra("Bundle", bundle);
             startActivity(intent);
         });
     }
